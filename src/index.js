2d8b686966f5f00c7f35135dc808189b74fdfcc9
Raushan7667 <kumarraushan.aic@gmail.com>
2025-02-16 19:33:38 +0530

    in redux provided to index file

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Toaster } from 'react-hot-toast';
+import rootReducer from "./reducer";
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+const store = configureStore({
+  reducer: rootReducer,
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
   <BrowserRouter>
     <App />
+    <Toaster />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
