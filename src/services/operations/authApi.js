612f955bf15b6789de7010f691f72b33fe1f8352
Raushan7667 <kumarraushan.aic@gmail.com>
2025-02-18 23:03:37 +0530

    login logout redux added

--- src/services/operations/authApi.js
+++ src/services/operations/authApi.js
@@ -78,3 +78,54 @@ const {
       toast.dismiss(toastId)
     }
   }
+  export function login(email, password, navigate) {
+    return async (dispatch) => {
+      const toastId = toast.loading("Loading...")
+      dispatch(setLoading(true))
+      
+      try {
+        const response = await apiConnector("POST", LOGIN_API, {
+          email,
+          password,
+        })
+  
+        console.log("LOGIN API RESPONSE............", response)
+  
+        if (!response.data.success) {
+          throw new Error(response.data.message)
+        }
+  
+        toast.success("Login Successful")
+        dispatch(setToken(response.data.token))
+        const userImage = response.data?.user?.image
+          ? response.data.user.image
+          : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.user.firstName} ${response.data.user.lastName}`
+        dispatch(setUser({ ...response.data.user, image: userImage }))
+        
+        localStorage.setItem("token", JSON.stringify(response.data.token))
+
+         const tokenData = {
+        value: response.data.token,
+        expires: Date.now() + 86400000, // 24 hours from now
+      };
+       localStorage.setItem("token", JSON.stringify(tokenData));
+        navigate("/")
+      } catch (error) {
+        console.log("LOGIN API ERROR............", error)
+        toast.error("Login Failed")
+      }
+      dispatch(setLoading(false))
+      toast.dismiss(toastId)
+    }
+  }
+  export function logout(navigate) {
+    return (dispatch) => {
+      dispatch(setToken(null))
+      dispatch(setUser(null))
+      dispatch(resetCart())
+      localStorage.removeItem("token")
+      localStorage.removeItem("user")
+      toast.success("Logged Out")
+      navigate("/")
+    }
+  }
