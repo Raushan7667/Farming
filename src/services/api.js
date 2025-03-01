8e84edcc3f1ffe7e9d2234bc00498d8d923663db
Raushan7667 <kumarraushan.aic@gmail.com>
2025-03-01 12:53:15 +0530

    product api added

--- src/services/api.js
+++ src/services/api.js
@@ -7,6 +7,28 @@ export const endpoints={
     RESETPASSTOKEN_API: BASE_URL + "/auth/resetpasswordtoken",
     RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
 }
+
+export const productEndpoint={
+    GET_PRODUCTS_API: BASE_URL + "/products",
+    GET_PRODUCT_API: BASE_URL + "/product",
+    ADD_PRODUCT_API: BASE_URL + "/product/add",
+    DELETE_PRODUCT_API: BASE_URL + "/product/delete",
+    UPDATE_PRODUCT_API: BASE_URL + "/product/update",
+    GET_PRODUCT_BY_ID_API: BASE_URL + "/products/getproductbyId/",
+    GET_PRODUCT_BY_CATEGORY_API: BASE_URL + "/products/category/",
+    GET_PRODUCT_BY_SELLER_API: BASE_URL + "/products/seller/",
+    GET_PRODUCT_BY_SEARCH_API: BASE_URL + "/products/search/",
+    GET_PRODUCT_BY_FILTER_API: BASE_URL + "/products/filter/",
+    GET_PRODUCT_BY_POPULAR_API: BASE_URL + "/products/popular",
+    GET_PRODUCT_BY_RELATED_API: BASE_URL + "/products/related/",
+    ADD_REVIEW_API: BASE_URL + "/product/review/add",
+    GET_REVIEW_API: BASE_URL + "/product/review/",
+    DELETE_REVIEW_API: BASE_URL + "/product/review/delete",
+    ADD_RATING_API: BASE_URL + "/product/rating/add",
+    GET_RATING_API: BASE_URL + "/product/rating/",
+    DELETE_RATING_API: BASE_URL + "/product/rating/delete",
+    
+}
 export const paymentEndpoint ={
     PRODUCT_PAYMENT_API: BASE_URL + "/order/createorder",
     PRODUCT_VERIFY_API: BASE_URL + "/",
